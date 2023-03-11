9622385733025f5bd1a9cdd62bfe2202180949ee
Refug1 <kirillezic@gmail.com>
2023-03-11 15:15:47 +0300

    Chapter 1 END

--- app/src/main/java/com/example/cleanarchitecture/domain/DeleteShopItemUseCase.kt
+++ app/src/main/java/com/example/cleanarchitecture/domain/DeleteShopItemUseCase.kt
@@ -1,8 +1,8 @@
 package com.example.cleanarchitecture.domain
 
-class DeleteShopItemUseCase {
+class DeleteShopItemUseCase(private val shopListRepository: ShopListRepository) {
 
     fun deleteShopItem(shopItem: ShopItem) {
-
+        shopListRepository.deleteShopItem(shopItem)
     }
 }
